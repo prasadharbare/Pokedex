6d53956d2a9529e271ba255b8e6d683a02f5bab3
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-10-02 08:07:37 +0000

    Pokemon function

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,23 @@ import data from "./data";
 
 // === DOM Selection ===
 const dataRow = document.querySelector("[data-row]");
-console.log(dataRow);
 
-for (let pokemon of data) {
+data.forEach(Pokemon);
+
+function Pokemon(data) {
   const div = document.createElement("div");
   div.classList.add("col");
   div.innerHTML = `<div class="card">
-    <img
-        src="${pokemon.image}"
-        class="card-img-top"
-        alt="${pokemon.name}"
-    />
-    <div class="card-body">
-        <h5 class="card-title">${pokemon.name}</h5>
-        <p class="card-text">${pokemon.description}</p>
-        <a href="${pokemon.link}" class="btn btn-warning">Visit</a>
-    </div>
-    </div>`;
+      <img
+          src="${data.image}"
+          class="card-img-top"
+          alt="${data.name}"
+      />
+      <div class="card-body">
+          <h5 class="card-title">${data.name}</h5>
+          <p class="card-text">${data.description}</p>
+          <a href="${data.link}" class="btn btn-warning">Visit</a>
+      </div>
+      </div>`;
   dataRow.appendChild(div);
 }
